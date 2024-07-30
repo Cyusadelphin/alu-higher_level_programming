69274fde00865b08af6a9370745190b30d289218
Cyusadelphin <d.cyusa@alustudent.com>
2024-07-30 08:19:20 +0000

    hello

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
-ends a POST request to the passed URL, and displays the body of the response.
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1" 
+#!/bin/bash
+# Send the POST request with the specified variables and display the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1" 
