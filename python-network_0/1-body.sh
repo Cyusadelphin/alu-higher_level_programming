68825174e4e1c94af68a71e821c2eefdd63e0497
Cyusadelphin <d.cyusa@alustudent.com>
2024-07-30 08:07:20 +0000

    hello

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,2 +1,3 @@
-ends a GET request to a URL, and displays the body of the response
-curl -sL "$1"  
+#!/bin/bash
+# Send the request using curl, and display the body of the response for 200 status code
+curl -s -L -I "$1" -w "%{http_code}" | grep "200" -q && curl -sL "$1" 
